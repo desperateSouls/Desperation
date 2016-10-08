b6ba7745328d10719665206cef244f6b8656dbc2
chrishieh <slapshot811@gmail.com>
2016-10-08 18:12:09 -0400

    Fixed things as a result of people not telling me what to do

--- Server Main.py
+++ Server Main.py
@@ -25,11 +25,12 @@ sock.listen(1)
 
 class StoreThingy:
 
-    def __init__(self, timestamp, sendie, sender, message):
+    def __init__(self, timestamp, sendie, sender, message, sent):
         self.timestamp = timestamp
         self.sendie = sendie
         self.sender = sender
         self.message = message
+        self.sent = sent
 
 class Server:
     def save_to_file(dump, serialized):
@@ -42,6 +43,18 @@ class Server:
         sendie_writer = csv.writer(sendie_data)
         sendie_writer.writerow(serialized)
 
+    def send_log(self, sendie):
+        log = []
+        sender_data = open(dump.sender + ".csv", 'w+')
+        sender_reader = csv.reader(sender_data)
+        sender_writer = csv.writer(sender_data)
+        for row in sender_reader:
+            message = row
+            message = pickle.load(message)
+            if message[1] == sendie and message[4] == False:
+                log.append(message)
+                sender_writer.writerow
+        return log
 
 while True:
     # Wait for a connection
@@ -58,14 +71,16 @@ while True:
             if data:
 
                 deserialized = pickle.loads(data, fix_imports=True, encoding="utf-8", errors="strict")
-                dump = StoreThingy(deserialized[0], deserialized[1], deserialized[2], deserialized[3])
-                serialized = pickle.dumps(dump)
+                if (type(deserialized) == tuple):
+                    dump = StoreThingy(deserialized[0], deserialized[1], deserialized[2], deserialized[3], False)
+                    serialized = pickle.dumps(dump)
+                    Server.save_to_file(dump, serialized)
+                elif (type(deserialized) == str):
+                    connection.sendall(Server.send_log(deserialized))
 
                 Server.save_to_file(dump, serialized)
                 print(deserialized)
-                # for number in deserialized:
-                #     print(number)
-                #if deserialized == codeword:
+
                 print(sys.stderr, 'sending data back to the client')
                 connection.sendall(data)
 
